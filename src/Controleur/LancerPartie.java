164d2a6194331ef21bebd937fbfe261e088239cd
Nylls_Gersan <boutotonyllsgersan@gmail.com>
2023-05-30 15:01:33 +0200

    mis à jour

--- src/Controleur/LancerPartie.java
+++ src/Controleur/LancerPartie.java
@@ -9,11 +9,11 @@ import java.util.Random;
  */
 public class LancerPartie {
     private Plateau plateau;
-    private MainMenu mainMenu;;
+    private MainMenu mainMenu;
     private Joueur joueur1;
     private Joueur joueur2;
-    private Random numero;// Identifaint d'un joueur 
-    private Random couleur;// Identifiant(unique) des unités d'un joueur 
+    private Random numero; // Identifiant d'un joueur
+    private Random couleur; // Identifiant(unique) des unités d'un joueur
 
     /**
      * Constructeur de la classe LancerPartie.
@@ -23,21 +23,39 @@ public class LancerPartie {
         plateau = new Plateau();
 
         // Création de l'interface graphique du plateau
-        mainMenu  = new MainMenu();
+        mainMenu = new MainMenu();
 
-        // 
-        joueur1 = new Joueur(mainMenu.getPseudo1(), numero.nextInt(3) + 1, couleur.nextInt(3) + 4);
-        joueur2 = new Joueur(mainMenu.getPseudo2(), numero.nextInt(3) + 4, couleur.nextInt(3) +1);
+        // Initialisation des générateurs aléatoires
+        numero = new Random();
+        couleur = new Random();
 
-        // Initialisation du plateau de jeu
-
-
-
-        // Mise à jour de la vue avec le plateau initial
+        // Affichage du menu principal
         mainMenu.setVisible(true);
+    }
 
+    /**
+     * Méthode pour lancer le jeu.
+     */
+    public void lancerJeu() {
+        // Vérification des pseudos des joueurs
+        String pseudo1 = mainMenu.getPseudo1();
+        String pseudo2 = mainMenu.getPseudo2();
 
-        // Affichage de l'interface graphique
+        if (pseudo1 != null && !pseudo1.isEmpty() && pseudo2 != null && !pseudo2.isEmpty()) {
+            // Création des joueurs avec des identifiants aléatoires
+            joueur1 = new Joueur(pseudo1, numero.nextInt(3) + 1, couleur.nextInt(3) + 4);
+            joueur2 = new Joueur(pseudo2, numero.nextInt(3) + 4, couleur.nextInt(3) + 1);
+
+            // Initialisation du plateau de jeu
+
+            // Mise à jour de la vue avec le plateau initial
+
+            // Lancement de l'interface graphique du plateau
+
+        } else {
+            // Affichage d'un message d'erreur si les pseudos sont vides ou non saisis
+            System.out.println("Veuillez saisir les pseudos des joueurs.");
+        }
     }
 
     /**
@@ -46,7 +64,7 @@ public class LancerPartie {
      */
     public static void main(String[] args) {
         LancerPartie jeu = new LancerPartie();
-        
+        jeu.lancerJeu();
     }
 }
 
