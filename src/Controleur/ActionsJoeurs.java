93c3912405df8054f536bcb63bbd80b7eea13df6
Nylls_Gersan <boutotonyllsgersan@gmail.com>
2023-05-31 11:45:13 +0200

    mis à jour

--- src/Controleur/ActionsJoeurs.java
+++ src/Controleur/ActionsJoeurs.java
@@ -1,4 +1,9 @@
 package Controleur;
+/*
+ * Permet de gerer les différentes actions d'un joueur
+ * telle que déplacer une unité, faire passer son tour, etc.
+ * 
+ */
 
 public class ActionsJoeurs {
     
--- src/Controleur/ActionsUnites.java
+++ src/Controleur/ActionsUnites.java
@@ -1,5 +1,11 @@
 package Controleur;
 
+/**
+ * Cette classe sert à gerer les déplacements & attaques d'une unité donnée
+ * En gros, elle met à jour les informations d'une unité.
+ * 
+ */
+
 public class ActionsUnites {
     
 }
